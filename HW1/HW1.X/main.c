97beb694734996dadf6314efdfbf80e317b4ab0f
michaeldoody <michaeldoody2017@u.northwestern.edu>
2016-04-10 23:38:08 -0500

    compile test
    test

--- HW1/HW1.X/main.c
+++ HW1/HW1.X/main.c
@@ -70,10 +70,3 @@ int main() {
 	    // use _CP0_SET_COUNT(0) and _CP0_GET_COUNT() to test the PIC timing
 		// remember the core timer runs at half the CPU speed
     }
-    
-    
-<<<<<<< HEAD
-}
-=======
-}
->>>>>>> origin/master
