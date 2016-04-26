fbdb53b955834bf3b755c2bc7cf562eaa79ae939
michaeldoody <michaeldoody2017@u.northwestern.edu>
2016-04-25 23:35:54 -0500

    functions
    added I2C2 functions from repo

--- HW4/HW4.X/main.c
+++ HW4/HW4.X/main.c
@@ -24,6 +24,66 @@ char SPI1_IO(char write) {
   return SPI4BUF;
 }
 
+void initSPI1()
+
+void initI2C2()
+
+void initExpander()
+
+setVoltage(char channel, char voltage)
+
+void setExpander(char pin, char level)
+
+char getExpander()
+
+// I2C Master utilities, 100 kHz, using polling rather than interrupts
+// The functions must be callled in the correct order as per the I2C protocol
+// Change I2C1 to the I2C channel you are using
+// I2C pins need pull-up resistors, 2k-10k
+
+void i2c_master_setup(void) {
+  I2C2BRG = some number for 100kHz;            // I2CBRG = [1/(2*Fsck) - PGD]*Pblck - 2 
+                                    // look up PGD for your PIC32
+  I2C2CONbits.ON = 1;               // turn on the I2C1 module
+}
+
+// Start a transmission on the I2C bus
+void i2c_master_start(void) {
+    I2C2CONbits.SEN = 1;            // send the start bit
+    while(I2C2CONbits.SEN) { ; }    // wait for the start bit to be sent
+}
+
+void i2c_master_restart(void) {     
+    I2C2CONbits.RSEN = 1;           // send a restart 
+    while(I2C2CONbits.RSEN) { ; }   // wait for the restart to clear
+}
+
+void i2c_master_send(unsigned char byte) { // send a byte to slave
+  I2C2TRN = byte;                   // if an address, bit 0 = 0 for write, 1 for read
+  while(I2C2STATbits.TRSTAT) { ; }  // wait for the transmission to finish
+  if(I2C2STATbits.ACKSTAT) {        // if this is high, slave has not acknowledged
+    // ("I2C2 Master: failed to receive ACK\r\n");
+  }
+}
+
+unsigned char i2c_master_recv(void) { // receive a byte from the slave
+    I2C2CONbits.RCEN = 1;             // start receiving data
+    while(!I2C2STATbits.RBF) { ; }    // wait to receive the data
+    return I2C2RCV;                   // read and return the data
+}
+
+void i2c_master_ack(int val) {        // sends ACK = 0 (slave should send another byte)
+                                      // or NACK = 1 (no more bytes requested from slave)
+    I2C2CONbits.ACKDT = val;          // store ACK/NACK in ACKDT
+    I2C2CONbits.ACKEN = 1;            // send ACKDT
+    while(I2C2CONbits.ACKEN) { ; }    // wait for ACK/NACK to be sent
+}
+
+void i2c_master_stop(void) {          // send a STOP:
+  I2C2CONbits.PEN = 1;                // comm is complete and master relinquishes bus
+  while(I2C2CONbits.PEN) { ; }        // wait for STOP to complete
+}
+
 // initialize spi4 and the ram module
 void ram_init() {
   // set up the chip select pin as an output
@@ -38,13 +98,13 @@ void ram_init() {
   // since the pic is just starting, we know that spi is off. We rely on defaults here
  
   // setup spi4
-  SPI4CON = 0;              // turn off the spi module and reset it
-  SPI4BUF;                  // clear the rx buffer by reading from it
-  SPI4BRG = 0x3;            // baud rate to 10 MHz [SPI4BRG = (80000000/(2*desired))-1]
-  SPI4STATbits.SPIROV = 0;  // clear the overflow bit
-  SPI4CONbits.CKE = 1;      // data changes when clock goes from hi to lo (since CKP is 0)
-  SPI4CONbits.MSTEN = 1;    // master operation
-  SPI4CONbits.ON = 1;       // turn on spi 4
+  SPI1CON = 0;              // turn off the spi module and reset it
+  SPI1BUF;                  // clear the rx buffer by reading from it
+  SPI1BRG = 0x3;            // baud rate to 10 MHz [SPI4BRG = (80000000/(2*desired))-1]
+  SPI1STATbits.SPIROV = 0;  // clear the overflow bit
+  SPI1CONbits.CKE = 1;      // data changes when clock goes from hi to lo (since CKP is 0)
+  SPI1CONbits.MSTEN = 1;    // master operation
+  SPI1CONbits.ON = 1;       // turn on spi 4
 
                             // send a ram set status command.
   CS = 0;                   // enable the ram
